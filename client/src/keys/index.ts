7451fc576f7b6b37527240f0c0f5f6d58d5b1989
Aleksandr Sidorov <a.sidorov@blaze.su>
2020-05-17 15:48:12 +0300

    FIX: client env

--- client/src/keys/index.ts
+++ client/src/keys/index.ts
@@ -7,6 +7,9 @@ const {
     PROCESS_ENV_HOST_IMAGE,
 } = publicRuntimeConfig;
 
+console.log("publicRuntimeConfig", publicRuntimeConfig)
+console.log("process.env", process.env)
+
 export const NODE_ENV = PROCESS_ENV_NODE_ENV;
 export const HOST_API = PROCESS_ENV_HOST_API;
 export const HOST_IMAGE = PROCESS_ENV_HOST_IMAGE;
