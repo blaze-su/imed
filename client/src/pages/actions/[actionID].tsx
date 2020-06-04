af08dc89836a9366b708fb32c0c987badfd3e166
Aleksandr Sidorov <a.sidorov@blaze.su>
2020-06-04 17:49:34 +0300

    add actions

--- client/src/pages/actions/[actionID].tsx
+++ client/src/pages/actions/[actionID].tsx
@@ -1,8 +1,8 @@
 import { Box, ErrorBoundary, Spinner, Title } from "@components/atoms";
+import { HOST_API, HOST_IMAGE_RESIZE } from "@keys";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { HOST_API } from "@keys";
 import { Layout } from "@components/template";
 import { SalesSlider } from "@components/organisms";
 import css from "./action.module.scss";
@@ -10,7 +10,7 @@ import { fetchSale } from "@redux/actions";
 import { useReset } from "@components/template/resetToggle";
 import { useRouter } from "next/router";
 
-const Sale = (props:any) => {
+const Sale = (props: any) => {
     const router = useRouter();
     const { actionID } = router.query;
     const url: string = `${HOST_API}/promos/${actionID}`;
@@ -18,12 +18,12 @@ const Sale = (props:any) => {
     useReset(dispatch);
     useEffect((): any => dispatch(fetchSale(url)), [url, dispatch]);
     const saleReducer = useSelector((store: any) => store.saleReducer);
-	const { sale, saleIsLoading } = saleReducer;
-	
-	const arr = [
+    const { sale, saleIsLoading } = saleReducer;
+
+    const arr = [
         { title: "Главная", link: "/" },
         { title: "Акции", link: "/actions" },
-        { title: sale.title, link: "" }
+        { title: sale.title, link: "" },
     ];
 
     return (
@@ -40,7 +40,17 @@ const Sale = (props:any) => {
                 ) : (
                     <ErrorBoundary>
                         <Title text={sale.title} />
-                        <section>Здесь текст акции!!!</section>
+                        <article>
+                            {sale.fileId ? (
+                                <img
+                                    src={`${HOST_IMAGE_RESIZE}/550x440/${sale.fileId.src}`}
+                                    alt={sale.title}
+                                    title={sale.title}
+                                />
+                            ) : null}
+
+                            <div dangerouslySetInnerHTML={{__html: sale.offer}} />
+                        </article>
                         <section>
                             <h2 className={css.title}>
                                 Рекомендуем посмотреть
