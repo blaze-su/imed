72b1248dcf2fc64910c990b1848ac8a8a8e626e0
Aleksandr Sidorov <a.sidorov@blaze.su>
2020-06-07 12:18:02 +0300

    fix slider

--- client/src/components/organisms/Banner/BannerSlider/index.tsx
+++ client/src/components/organisms/Banner/BannerSlider/index.tsx
@@ -1,68 +1,77 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { Box, Spinner } from "@components/atoms";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { HOST_API } from "@keys"
-import { IPhoto } from '@interfaces';
-import Slider from 'react-slick';
-import { Spinner } from '@components/atoms';
-import { fetchBanners } from '@redux/actions';
-import style from './BannerSlider.module.scss';
+import { HOST_API } from "@keys";
+import { IPhoto } from "@interfaces";
+import Slider from "react-slick";
+import { fetchBanners } from "@redux/actions";
+import style from "./BannerSlider.module.scss";
 
 export interface IBanner {
-	_id: string;
-	alt?: string;
-	title: string;
-	text: string;
-	photo: IPhoto | string;
+    _id: string;
+    alt?: string;
+    title: string;
+    text: string;
+    photo: IPhoto | string;
 }
 
 const settings = {
-	dots: true,
-	infinite: true,
-	slidesToScroll: 1,
-	slidesToShow: 1,
-	speed: 300,
-	responsive: [
-		{
-			breakpoint: 541,
-			settings: {
-				dots: false
-			}
-		}
-	]
+    dots: true,
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    speed: 300,
+    responsive: [
+        {
+            breakpoint: 541,
+            settings: {
+                dots: false,
+            },
+        },
+    ],
 };
 
 export const BannerSlider = () => {
-	const url: string = `${HOST_API}/banners`;
+    const url: string = `${HOST_API}/banners`;
 
-	const dispatch = useDispatch();
-	useEffect((): any => dispatch(fetchBanners(url)), [url, dispatch]);
-	const bannerReducer = useSelector((store: any) => store.bannerReducer);
-	const { banners, bannerIsLoading } = bannerReducer;
+    const dispatch = useDispatch();
+    useEffect((): any => dispatch(fetchBanners(url)), [url, dispatch]);
+    const bannerReducer = useSelector((store: any) => store.bannerReducer);
+    const { banners, bannerIsLoading } = bannerReducer;
 
-	return (
-		<section className={style.bannerWrap}>
-			{bannerIsLoading ? (
-				<Spinner />
-			) : (
-				<Slider ref={slider => slider} {...settings}>
-					{banners.map(({ alt, title, text, _id, photo }: IBanner) => (
-						<div className={style.item} key={_id}>
-							<div className={style.info}>
-								<h1 className={style.title}>{title}</h1>
-								<p className={style.desc}>{text}</p>
-							</div>
-							<img
-								className={style.img}
-								src={ typeof photo === 'string' ? photo : photo.src }
-								alt={alt}
-								title={title}
-							/>
-						</div>
-					))}
-				</Slider>
-			)}
-		</section>
-	);
+    return (
+        <section className={style.slider}>
+            {bannerIsLoading ? (
+                <Spinner />
+            ) : (
+                <Slider ref={(slider) => slider} {...settings}>
+                    {banners.map(
+                        ({ alt, title, text, _id, photo }: IBanner) => (
+                            <div className={style.item} key={_id}>
+                                <div className={style.box}>
+                                    <div className={style.info}>
+                                        <h1 className={style.title}>{title}</h1>
+                                        <p className={style.desc}>{text}</p>
+                                    </div>
+                                    <div className={style.image}>
+                                        <img
+                                            className={style.img}
+                                            src={
+                                                typeof photo === "string"
+                                                    ? photo
+                                                    : photo.src
+                                            }
+                                            alt={alt}
+                                            title={title}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    )}
+                </Slider>
+            )}
+        </section>
+    );
 };
-
